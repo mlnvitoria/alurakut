cacc62cf6a387eefd99919626fb80453e57a5837
Vitoria Mendes <vnunes@speedtocontact.com>
2021-07-15 14:19:43 -0300

    Adicionando componente AlurakutProfileSidebarMenuDefault na página index

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import Box from "./src/components/Box";
 import MainGrid from "./src/components/MainGrid";
 import { ProfileRelationsBoxWrapper } from "./src/components/ProfileRelations";
-import { AlurakutMenu, OrkutNostalgicIconSet } from "./src/lib/AlurakutCommons";
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from "./src/lib/AlurakutCommons";
 
 function ProfileSidebar(properties) {
   return (
     <Box>
       <img src={`https://github.com/${properties.githubUser}.png`}  style={{ borderRadius: '8px' }} />
+      <hr />
+      <p>
+        <a className="boxLink" href={`https://github.com/${properties.githubUser}`}>
+          @{properties.githubUser}
+        </a>
+      </p>
+      <hr />
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   );
 }
@@ -38,7 +46,7 @@ export default function Home() {
             <OrkutNostalgicIconSet></OrkutNostalgicIconSet>
           </Box>
         </div>
-        
+
         <div className="peopleArea" style={{ gridArea: "peopleArea" }}>
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">Pessoas ({people.length})</h2>
